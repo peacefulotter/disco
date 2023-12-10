1deb75b45b5132bb074c0773929cb7e3618b27b6
peacefulotter <zankro67@gmail.com>
2023-12-10 15:42:53 +0100

    Fixed last imports error

--- discojs/discojs-core/src/index.node.ts
+++ discojs/discojs-core/src/index.node.ts
@@ -12,20 +12,10 @@ export * as aggregator from './aggregator'
 export { WeightsContainer, aggregation } from './weights'
 export { AsyncInformant } from './async_informant'
 export { Logger, ConsoleLogger, TrainerLog } from './logging'
-export {
-    Memory,
-    ModelType,
-    ModelInfo,
-    Path,
-    ModelSource,
-    Empty as EmptyMemory,
-} from './memory'
-export {
-    Disco,
-    TrainingSchemes,
-    TrainingFunction,
-    fitModelFunctions,
-} from './training'
+export type { ModelInfo, Path, ModelSource } from './memory'
+export { Memory, ModelType, Empty as EmptyMemory } from './memory'
+export type { TrainingFunction } from './training'
+export { Disco, TrainingSchemes, fitModelFunctions } from './training'
 export { Validator } from './validation'
 
 export * from './task'
