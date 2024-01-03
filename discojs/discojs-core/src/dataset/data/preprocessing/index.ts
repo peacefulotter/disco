30f9ff08f6018c5065c4b15dffffd9ae452afd48
peacefulotter <zankro67@gmail.com>
2024-01-03 16:42:07 +0100

    Fixed various type exports

--- discojs/discojs-core/src/dataset/data/preprocessing/index.ts
+++ discojs/discojs-core/src/dataset/data/preprocessing/index.ts
@@ -1,4 +1,13 @@
-export { Preprocessing, PreprocessingFunction } from './base'
-export { AVAILABLE_PREPROCESSING as IMAGE_PREPROCESSING, ImagePreprocessing } from './image_preprocessing'
-export { AVAILABLE_PREPROCESSING as TABULAR_PREPROCESSING, TabularPreprocessing } from './tabular_preprocessing'
-export { AVAILABLE_PREPROCESSING as TEXT_PREPROCESSING, TextPreprocessing } from './text_preprocessing'
+export { type Preprocessing, type PreprocessingFunction } from './base'
+export {
+    AVAILABLE_PREPROCESSING as IMAGE_PREPROCESSING,
+    ImagePreprocessing,
+} from './image_preprocessing'
+export {
+    AVAILABLE_PREPROCESSING as TABULAR_PREPROCESSING,
+    TabularPreprocessing,
+} from './tabular_preprocessing'
+export {
+    AVAILABLE_PREPROCESSING as TEXT_PREPROCESSING,
+    TextPreprocessing,
+} from './text_preprocessing'
--- discojs/discojs-core/src/memory/index.ts
+++ discojs/discojs-core/src/memory/index.ts
@@ -1,3 +1,4 @@
 export { Empty } from './empty'
-export { Memory, ModelInfo, Path, ModelSource } from './base'
+export { Memory } from './base'
+export type { ModelInfo, Path, ModelSource } from './base'
 export { ModelType } from './model_type'
