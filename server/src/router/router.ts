b3070d708ae86340798d3531808df541e7884e70
peacefulotter <zankro67@gmail.com>
2024-01-04 12:29:54 +0100

    Fixed server imports and wrong model types used (tfjs.LayersModel -> training.model.Model)

--- server/src/router/router.ts
+++ server/src/router/router.ts
@@ -9,42 +9,42 @@ import { Decentralized } from './decentralized'
 import { Tasks } from './tasks'
 
 export class Router {
-  // TODO choose between federated and/or decentralized
+    // TODO choose between federated and/or decentralized
 
-  private readonly ownRouter: expressWS.Router
+    private readonly ownRouter: expressWS.Router
 
-  constructor (
-    wsApplier: expressWS.Instance,
-    private readonly tasksAndModels: TasksAndModels,
-    private readonly config: Config
-  ) {
-    const tasks = new Tasks(this.config, this.tasksAndModels)
-    const federated = new Federated(wsApplier, this.tasksAndModels)
-    const decentralized = new Decentralized(wsApplier, this.tasksAndModels)
+    constructor(
+        wsApplier: expressWS.Instance,
+        private readonly tasksAndModels: TasksAndModels,
+        private readonly config: Config
+    ) {
+        const tasks = new Tasks(this.config, this.tasksAndModels)
+        const federated = new Federated(wsApplier, this.tasksAndModels)
+        const decentralized = new Decentralized(wsApplier, this.tasksAndModels)
 
-    this.ownRouter = express.Router()
-    wsApplier.applyTo(this.ownRouter)
+        this.ownRouter = express.Router()
+        wsApplier.applyTo(this.ownRouter)
 
-    process.nextTick(() =>
-      wsApplier.getWss().on('connection', (ws, req) => {
-        if (!federated.isValidUrl(req.url) && !decentralized.isValidUrl(req.url)) {
-          console.log('Connection refused')
-          ws.terminate()
-          ws.close()
-        }
-      })
-    )
+        process.nextTick(() =>
+            wsApplier.getWss().on('connection', (ws: any, req: any) => {
+                if (!federated.isValidUrl(req.url) && !decentralized.isValidUrl(req.url)) {
+                    console.log('Connection refused')
+                    ws.terminate()
+                    ws.close()
+                }
+            })
+        )
 
-    this.ownRouter.get('/', (_, res, next) => {
-      res.send('Server for DeAI & FeAI \n')
-      next()
-    })
-    this.ownRouter.use('/deai', decentralized.router)
-    this.ownRouter.use('/feai', federated.router)
-    this.ownRouter.use('/tasks', tasks.router)
-  }
+        this.ownRouter.get('/', (_, res, next) => {
+            res.send('Server for DeAI & FeAI \n')
+            next()
+        })
+        this.ownRouter.use('/deai', decentralized.router)
+        this.ownRouter.use('/feai', federated.router)
+        this.ownRouter.use('/tasks', tasks.router)
+    }
 
-  get router (): express.Router {
-    return this.ownRouter
-  }
+    get router(): express.Router {
+        return this.ownRouter
+    }
 }
